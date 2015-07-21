bf4291d91ff8639b0b68fb552ddd8441d6a73532
dprzywara <dariusz.przywaraa@gmail.com>
2015-07-21 15:44:23 +0200

    wersja 2

--- GitTesty/src/Main.java
+++ GitTesty/src/Main.java
@@ -4,7 +4,13 @@ public class Main {
 	
 	private int zmienna1;
 	//zmiana z githube
-	int nowa zmienna;
+	int zm2;
+	
+	
+	//wersja druga na stash
+	int add(int a , int b){
+		return a+b;
+	}
 	
 
 }
