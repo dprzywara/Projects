4d5313dc1658e3d943bb879da1becffa1615f7eb
dprzywara <dariusz.przywaraa@gmail.com>
2015-07-21 16:04:42 +0200

    usuniecie .class

--- GitTesty/src/Main.java
+++ GitTesty/src/Main.java
@@ -2,30 +2,18 @@
 public class Main {
 	
 	
-	private int zmienna1;
-	//zmiana z githube
+	private int zm1;
 	int zm2;
 	
 	int nowa_zmienna;
-	//wersja druga na stash
 	int add(int a , int b){
-		return a+b;
+		return a+b+zm1;
 	}
-	
-	
-	///fsagbdhkbashgbashkgd
-	
 	
 	void metoda(){
-		
-		///gsdgfsdg
-		//fasfas
 		nowa_zmienna++;
 	}
-	
-	
-	///fsagbdhkbashgbashkgd
-	
+
 	
 
 
